fa4cc56f067bd0e972ad689bbd0465f7b8ba79ba
JairusTumacder <tumacderjairus7@gmail.com>
2023-02-03 18:13:01 -1000

    PID Value Changes

--- src/main/java/frc/robot/subsystems/PivotArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotArmSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import javax.swing.JPopupMenu.Separator;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
@@ -13,28 +15,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
 
     //////////////////
-    //  Variables  //
-    ////////////////
+   //  Variables  ///
+  //////////////////
     private final TalonSRX right = new TalonSRX(4);
     private double kp;
     private double ki;
     private double kd;
-    private final PIDController pid = new PIDController(kp, ki, kd);
+    private final PIDController pid = new PIDController(0.0005, 0.05, 0.000006);
     private final TalonEncoder tEnc;
     private double setpoint;
 
 
     ////////////////////////////////////////
-    //  Pivot Arm Subsystem Constructor  //
-    //////////////////////////////////////
+   //  Pivot Arm Subsystem Constructor  ///
+  ////////////////////////////////////////
     public PivotArmSubsystem(TalonEncoder enc){ // Instantiates the Talon Encoder variable
         tEnc = enc;
         pid.setTolerance(5);
     }
 
     ////////////////////////
-    //  Encoder Methods  //
-    //////////////////////
+   //  Encoder Methods  ///
+  ////////////////////////
     public double getEncoder(){ // Return the Encoder Value
        return right.getSensorCollection().getQuadraturePosition();
     }
@@ -44,8 +46,8 @@ public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
     }
 
     ////////////////////////////////
-    //  Set Pivot Speed Methods  //
-    //////////////////////////////
+   //  Set Pivot Speed Methods  ///
+  ////////////////////////////////
     public void pivotUp(DoubleSupplier speed){ // Pivots the arm up based on its speed
         right.set(ControlMode.PercentOutput, speed.getAsDouble());
     }
@@ -59,8 +61,8 @@ public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
     }
 
     /////////////////////////
-    // Pivot PID Methods  //
-    /////////////////////// 
+   // Pivot PID Methods  ///
+  ///////////////////////// 
     public void compareErrors(){ // Resets the Integral Term if it reaches a certain limit
         double before = pid.getPositionError(); 
         double after = pid.getPositionError();
@@ -76,15 +78,15 @@ public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
     }
 
     public double calcP(double setpoint){ // Calculates the value of the Porportional Term by multiplying the error (setpoint - encoder) by the kP constantSets the limit of the error
-        double error = pid.getPositionError();
+        double error = pid.calculate(getEncoder(), setpoint);
         if(pid.atSetpoint()){ // If the PID is at the setpoint, return a value of 0
             return 0;
         }
-        if(error > 0.2){ // If the error is greater than a limit of 0.2, return a value of 0.2
-            return 0.2;
+        if(error > 0.5){ // If the error is greater than a limit of 0.2, return a value of 0.2
+            return 0.5;
         }
-        else if(error < -0.2){ // If the error is less than a limit of -0.2, return a value of -0.2
-            return -0.2;
+        else if(error < -0.5){ // If the error is less than a limit of -0.2, return a value of -0.2
+            return -0.5;
         }
         else{ // If everything else fails, return the error 
             return error;
@@ -94,11 +96,11 @@ public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
     public double calcD(double setpoint){ // Calculates the value of the Derivative Term by multiplying the error rate by the kD constant
         double before = pid.getPositionError();
         double after = pid.getPositionError();
-        if((after - before) > 0.2){ // If the error rate (error - previous error) is greater than a limit of 0.2, return a value of 0.2
-            return 0.2;
+        if((after - before) > 0.5){ // If the error rate (error - previous error) is greater than a limit of 0.2, return a value of 0.2
+            return 0.5;
         }
-        else if((after - before) < -0.2){ // If the error rate is less than a limit of -0.2, return a value of -0.2
-            return -0.2;
+        else if((after - before) < -0.5){ // If the error rate is less than a limit of -0.2, return a value of -0.2
+            return -0.5;
         }
         else{ // If everything else fails, return the error
             return pid.calculate(getEncoder(), setpoint); 
@@ -112,8 +114,8 @@ public class PivotArmSubsystem extends SubsystemBase{ // Pivot Arm Subsystem
     }
     
     ///////////////////////
-    //  Printing Method //
-    /////////////////////
+   //  Printing Method ///
+  ///////////////////////
     @Override
     public void periodic(){ // Prints and edits kp, ki, and kd so you do not have to redeploy and edit code
         SmartDashboard.putNumber("Pivot Arm Encoder: ", getEncoder());
