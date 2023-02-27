7babd1435170c47d4fbf199fd65e1e9f38d03b47
JairusTumacder <tumacderjairus7@gmail.com>
2023-02-26 15:11:09 -1000

    Update Changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,9 @@ import frc.robot.commands.TuckedFromTop;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.PivotArmSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.Tucked;
 
 public class RobotContainer {
   private final PivotArmSubsystem p_subsystem = new PivotArmSubsystem(); 
@@ -21,6 +20,8 @@ public class RobotContainer {
 
   public RobotContainer() {
    // p_subsystem.setDefaultCommand(new PivotArmJoystickCmd(p_subsystem, () -> xController.getLeftY()));
+
+
     configureBindings();
   }
 
@@ -29,15 +30,10 @@ public class RobotContainer {
     new JoystickButton(xController, 2).onTrue(new TuckedFromBottom(p_subsystem, elevator));
     new JoystickButton(xController, 3).onTrue(new TopNode(p_subsystem, elevator)); // Button for the middle position
     new JoystickButton(xController, 4).onTrue(new TuckedFromTop(p_subsystem, elevator)); // Button for the high position
-    new JoystickButton(xController, 5).onTrue(new ProxyCommand(() -> {
-      SmartDashboard.putNumber("Select command", elevator.getEncoder());
-      if (elevator.getEncoder() < 160) {
-        return new TuckedFromBottom(p_subsystem, elevator);
-      } else {
-        return new TuckedFromTop(p_subsystem, elevator);
-      }}
-    ));
+    new JoystickButton(xController, 5).onTrue(Tucked.getCommand(p_subsystem, elevator));
   }
+
+  
 
   public Command getAutonomosCommand() {
     return null;
